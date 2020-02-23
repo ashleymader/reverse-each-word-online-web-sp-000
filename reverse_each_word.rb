d96b00f746036ab305efcd9804e195c75042fac7
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-23 17:08:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,7 @@
 def reverse_each_word("Hello there, and how are you?")
   
+  
+  
+famous_wizards = ["Dumbledore", "Gandalf", "Merlin"]
+famous_wizards.reverse
+  => ["Merlin", "Gandalf", "Dumbledore"]
